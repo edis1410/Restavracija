c3ae64edd4dadd8fcac5057ad8ccfca1a989b707
enesf00 <ef1816@student.uni-lj.si>
2021-12-17 11:24:01 +0100

    Osnovna funkcionalnost

--- app/src/main/java/com/example/restavracija/MainActivity.java
+++ app/src/main/java/com/example/restavracija/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.restavracija;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    private static int CAMERA_REQUEST_CODE = 101;
+    String[] tab = {Manifest.permission.CAMERA};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupPermission();
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    private void setupPermission(){
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest();
+        }
+    }
+
+    private void makeRequest(){
+        ActivityCompat.requestPermissions(this, tab, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+        if (requestCode == CAMERA_REQUEST_CODE){
+            if (grantResults == null || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Vpali kamero", Toast.LENGTH_SHORT);
+            }
+            else {
+                //Uspesna
+            }
+        }
     }
 }
